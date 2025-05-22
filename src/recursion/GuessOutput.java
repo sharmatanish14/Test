36195006c9093f014f846f4bfaff9bda3d193899
Tanish <no email>
2025-05-22 21:07:30 +0530

    add recursion

---
+++ src/recursion/GuessOutput.java
@@ -0,0 +1,19 @@
+package recursion;
+
+public class GuessOutput {
+
+    public static void main(String[] args) {
+        fun(12);
+    }
+
+    // this function is doing decimal ti binary conversion
+    public static void fun(int n){
+        if(n==0){
+            return;
+        }
+
+        fun(n/2);
+
+        System.out.print(n%2);
+    }
+}
